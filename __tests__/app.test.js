351b9f56cba4d3b2b2cfd108f4283516909b1edc
ConCat99 <concatgithub@gmail.com>
2025-03-06 12:33:22 +0000

    completed treasure sort

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,85 +5,78 @@ const app = require('../app');
 const request = require('supertest');
 
 beforeEach(() => {
-  return seed(testData);
+	return seed(testData);
 });
 
 afterAll(() => {
-  return db.end();
+	return db.end();
 });
 
 // GET /api/treasures
 // Create an endpoint that responds with all treasures, including the shop name and details, to allow Mitch to view all the treasures currently available.
-
-// each treasure should have the following keys:
-// treasure_id
-// treasure_name
-// colour
-// age
-// cost_at_auction
-// shop_name
 // default sort criteria: age
 // default sort order: ascending
 // /api/treasures, first result should be the youngest (default)
 
 {
-  treasures: [
-    {
-      // treasure_id
-      // treasure_name
-      // colour
-      // age
-      // cost_at_auction
-      // shop_name
-    },
-  ];
+	treasures: [
+		{
+			// treasure_id
+			// treasure_name
+			// colour
+			// age
+			// cost_at_auction
+			// shop_name
+		},
+	];
 }
 
-describe('GET: /api/obiiscoolerthantufty', () => {
-  test.todo('test what happens for an undefined endpoint', () => {});
-});
-
 describe('GET: /api/treasures', () => {
-  test('200: responds with all treasures, including the shop name and details', () => {
-    return request(app)
-      .get('/api/treasures')
-      .expect(200)
-      .then(({ body }) => {
-        console.log(body, '<<<<<<< body');
-        expect(body.treasures.length).toBe(26);
-        body.treasures.forEach(
-          ({
-            treasure_id,
-            treasure_name,
-            colour,
-            age,
-            cost_at_auction,
-            shop_name,
-          }) => {
-            expect(typeof treasure_id).toBe('number');
-            expect(typeof treasure_name).toBe('string');
-            expect(typeof colour).toBe('string');
-            expect(typeof age).toBe('number');
-            expect(typeof cost_at_auction).toBe('number');
-            expect(typeof shop_name).toBe('string');
-          }
-        );
-      });
-  });
-  test('200: responds with treasures ordered according to the order query', () => {
-    // /api/treasures?sort_by=age
-    // I don't know if or when you will need this!!!
-  });
+	test('200: responds with all treasures, including the shop name and details', () => {
+		return request(app)
+			.get('/api/treasures')
+			.expect(200)
+			.then(({ body }) => {
+				expect(body.treasures.length).toBe(26);
+				body.treasures.forEach(
+					({
+						treasure_id,
+						treasure_name,
+						colour,
+						age,
+						cost_at_auction,
+						shop_name,
+					}) => {
+						expect(typeof treasure_id).toBe('number');
+						expect(typeof treasure_name).toBe('string');
+						expect(typeof colour).toBe('string');
+						expect(typeof age).toBe('number');
+						expect(typeof cost_at_auction).toBe('number');
+						expect(typeof shop_name).toBe('string');
+					}
+				);
+			});
+	});
+	test.only('200: responds with treasures ordered by ascending', () => {
+		return request(app)
+			.get('/api/treasures')
+			.expect(200)
+			.then(({ body }) => {
+				const treasuresCopy = [...body.treasures].sort((a, b) => a.age - b.age);
+				expect(body.treasures).toEqual(treasuresCopy);
+			});
+	});
 });
 
-describe('GET: /api/treasures/:treasure_id', () => {
-  test.todo('test the happy path with a parameter', () => {
-    // test logic goes here!
-  });
-  test.todo('id is not a number', () => {
-    // send a request where the id is not a number
-  });
-  test.todo('request a resource that doesnt exist', () => {
-    // maybe request treasure 99
-  });
+xdescribe('GET: /api/treasures/:treasure_id', () => {
+	test('200: Responds with the correct treasure that is linked to the endpoint', () => {
+		// return request(app)
+		//   .get(/api/treasures/3)
+	});
+	test('id is not a number', () => {
+		// send a request where the id is not a number
+	});
+	test('request a resource that doesnt exist', () => {
+		// maybe request treasure 99
+	});
 });
--- models/treasures.models.js
+++ models/treasures.models.js
@@ -1,14 +1,15 @@
 const db = require('../db/connection');
 
 exports.selectTreasures = () => {
-  return db
-    .query(
-      `
+	return db
+		.query(
+			`
     SELECT treasure_id, treasure_name, colour, age, cost_at_auction, shop_name FROM treasures
-    JOIN shops
-    ON treasures.shop_id = shops.shop_id;`
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+    JOIN shops  ON treasures.shop_id = shops.shop_id
+    ORDER BY age ASC
+    ;`
+		)
+		.then(({ rows }) => {
+			return rows;
+		});
 };
